8f1af81bfd4cbcfb4274c0d4e601ec2906c38d86
Вадим Дубровин <vy831158@gmail.com>
2022-03-19 20:22:00 +0300

    Initical comit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //входные данные
+        int Tiketprice = 3000; // стоимость билета
+
+        int Bonus = 20; // кратность начисление бонусов
+        //лолгика программы
+        System.out.println(" Cумма бонусов составляет: ");
+        System.out.println(Tiketprice/Bonus );
+
+
+    }
+}
