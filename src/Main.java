afa164f0f43a6669fac86870577fb1b5a5af5645
Вадим Дубровин <vy831158@gmail.com>
2022-04-05 23:21:37 +0300

    Pravka

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         //входные данные
-        int Tiketprice = 3000; // стоимость билета
-
-        int Bonus = 20; // кратность начисление бонусов
+        int tiketprice = 3000; // стоимость билета
+        int bonus = 20; // кратность начисление бонусов
         //лолгика программы
-        System.out.println(" Cумма бонусов составляет: ");
-        System.out.println(Tiketprice/Bonus );
-
-
+        System.out.println("Cумма бонусов составляет:");
+        System.out.println(tiketprice / bonus);
     }
 }
